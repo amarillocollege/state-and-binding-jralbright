6029787f0ba43a3c107cfdbe39b77e579ddf07b3
joseph albright <josephrandallalbright@gmail.com>
2023-11-15 12:44:43 -0600

    2nd Commit

--- State and Binding/ContentView.swift
+++ State and Binding/ContentView.swift
@@ -12,18 +12,21 @@ struct ContentView: View {
     @State var counter1 = 0
     @State var counter2 = 0
     @State var counter3 = 0
-    
+        
     var body: some View {
         VStack {
             
-            
-            
+            Text("\(counter1 + counter2 + counter3)")
+                .font(.system(size: 100, weight: .bold, design: .rounded))
             HStack{
                 CounterButton(counter: $counter1, color: .blue)
                 CounterButton(counter: $counter2, color: .green)
                 CounterButton(counter: $counter3, color: .red)
             }
         }
+        //.frame(width: 200, height: 200)
+        .foregroundStyle(.black)
+        .background(.white)
     }
 }
 
@@ -39,7 +42,7 @@ struct CounterButton: View {
             counter = counter + 1
         } label: {
             Circle()
-                .frame(width: 200, height: 200)
+                .frame(width: 100, height: 100)
                 .foregroundStyle(color)
                 .overlay {
                     Text("\(counter)")
