1a548adfa6b047a1a6f70705873a988b42009ef6
joseph albright <josephrandallalbright@gmail.com>
2023-11-15 10:50:02 -0600

    1st Commit

--- State and Binding/ContentView.swift
+++ State and Binding/ContentView.swift
@@ -2,34 +2,50 @@
 //  ContentView.swift
 //  State and Binding
 //
-//  Created by [YOUR NAME GOES HERE]
+//  Created by Joseph Albright
 //
 
 import SwiftUI
 
 
 struct ContentView: View {
-    var counter = 1
+    @State var counter1 = 0
+    @State var counter2 = 0
+    @State var counter3 = 0
     
     var body: some View {
         VStack {
-            Button {
-                counter += 1
-            } label: {
-                Circle()
-                    .frame(width: 200, height: 200)
-                    .foregroundStyle(.red)
-                    .overlay {
-                        Text("\(counter)")
-                            .font(.system(size: 100, weight: .bold, design: .rounded))
-                            .foregroundStyle(.white)
-                    }
-            }
             
+            
+            
+            HStack{
+                CounterButton(counter: $counter1, color: .blue)
+                CounterButton(counter: $counter2, color: .green)
+                CounterButton(counter: $counter3, color: .red)
+            }
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct CounterButton: View {
+    @Binding var counter: Int
+    var color: Color
+    var body: some View {
+        Button {
+            counter = counter + 1
+        } label: {
+            Circle()
+                .frame(width: 200, height: 200)
+                .foregroundStyle(color)
+                .overlay {
+                    Text("\(counter)")
+                        .font(.system(size: 100, weight: .bold, design: .rounded))
+                        .foregroundStyle(.white)
+                }
+        }
+    }
 }
